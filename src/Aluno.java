cb64aab0623b70819a8014ded7c666d85f83c23a
Lucas06-ux <lubgian@gmail.com>
2025-04-04 22:55:20 -0300

    arrumando o código

--- src/Aluno.java
+++ src/Aluno.java
@@ -2,25 +2,24 @@ public class Aluno {
 
     String nome;
     int idade;
-    float rm;
+    String rm;
     double cp;
     double cp2;
     double cp3;
     double challenge;
     double gs;
-    double mediaCp;
 
 
 
 
-    double calcularMedia(){
-
-        double notaFinal;
-        notaFinal = mediaCp * 0.2 + challenge * 0.2 + gs * 0.6;
-        return notaFinal;
+    double calcularMediaCp(){
+        return (cp + cp2 + cp3) /3;
     }
 
+    double calcularMediaFinal(){
+        return calcularMediaCp() * 0.2 + challenge * 0.2 + gs * 0.6;
 
+    }
 
 
 
--- src/Principal.java
+++ src/Principal.java
@@ -4,49 +4,39 @@ public class Principal {
 
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
+        Aluno aluno1 = new Aluno();
 
         System.out.println("Digite o nome do aluno");
-        String nome = leitor.next() + leitor.nextLine();
+        aluno1.nome = leitor.next() + leitor.nextLine();
 
         System.out.println("Digite a idade");
-        int idade = leitor.nextInt();;
+        aluno1.idade = leitor.nextInt();;
 
         System.out.println("Digite o rm do aluno");
-        float rm = leitor.nextFloat();
+        aluno1.rm = leitor.next() + leitor.nextLine();
 
         System.out.println("Digite a nota da cp1");
-        double cp = leitor.nextDouble();
+        aluno1.cp = leitor.nextDouble();
 
         System.out.println("Digite a nota da cp2");
-        double cp2 = leitor.nextDouble();
+        aluno1.cp2 = leitor.nextDouble();
 
         System.out.println("Digite a nota da cp3");
-        double cp3 = leitor.nextDouble();
-
-        double mediaCp = (cp + cp2 + cp3) /3;
+        aluno1.cp3 = leitor.nextDouble();
 
         System.out.println("Digite a nota do challenge");
-        double challenge = leitor.nextDouble();
+        aluno1.challenge = leitor.nextDouble();
 
         System.out.println("Por fim, digite a nota da gs");
-        double gs = leitor.nextDouble();
+        aluno1.gs = leitor.nextDouble();
 
 
+      double media =  aluno1.calcularMediaFinal();
+      double mediaCp = aluno1.calcularMediaCp();
+        System.out.println("O aluno " + aluno1.nome +" ficou com a média final de nota " + media + ", e a média dos checkpoints ficou " + mediaCp);
 
-        Aluno aluno1 = new Aluno();
-        aluno1.nome = nome;
-        aluno1.idade = idade;
-        aluno1.rm = rm;
-        aluno1.cp = cp;
-        aluno1.cp2 = cp2;
-        aluno1.cp3 = cp3;
-        aluno1.challenge = challenge;
-        aluno1.gs = gs;
-
-
-      double media =  aluno1.calcularMedia();
-
-        System.out.println("A média final ficou " + media);
+        System.out.printf("Média do cp formatado: %.2f ", mediaCp);
+        System.out.printf("\nMédia semestre formatado: %.2f", media);
 
 
     }
